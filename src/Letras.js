d3e2b5b89e8cc92532725d3c167589e56e38f784
Theo Piedade <33207995+theopiedade@users.noreply.github.com>
2023-05-15 17:43:21 -0300

    Update Letras.js

--- src/Letras.js
+++ src/Letras.js
@@ -1,27 +1,58 @@
 import React from "react";
+import App from "./App";
 
-const alfabeto = [
+
+export const alfabeto = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
 ];
 
-const status = [
-    "true", "true", "true", "true", "true", "true", "true", "true", 
-    "true", "true", "true", "true", "true", "true", "true", "true", 
-    "true", "true", "true", "true", "true", "true", "true", "true",
-    "true", "true"
-];
+let status_disabled = [ ];
 
-export default function Letras() {
+export default function Letras({obj, setobj}) {
+    
+    console.log("Letras acionado, obj.status = "+obj.status);
+    
+    if (obj.status === "iniciado") setarDisabled(false);
+    else { setarDisabled(true); }
+
+    function setarDisabled(acao) {
+        console.log("Setar disabled acionado com a ação: "+acao);
+        alfabeto.map((i, n) => {
+             obj.statusBotoesLetras[n] = acao; 
+             console.log("Letra: "+i+" = "+obj.statusBotoesLetras[n]);
+            })
+    }
+
+    function checaStatus(n) {
+        console.log("Checa Status acionado "+obj.statusBotoesLetras[n]);
+        if (obj.statusBotoesLetras[n] === true) return "botao_letra_desabilitado";
+        else return "botao_letra_habilitado";
+    }
+    setobj(obj);
     return (
         <div class="container_botoes_letras">
             {
-            alfabeto.map((i) => ( 
+            alfabeto.map((i, n) => ( 
                 <input type="button" 
-                 class={status[i] === "true" ? botao_letra_desabilitado : botao_letra_habilitado}
-                 value={alfabeto[i]} disabled={status[i]}></input>
+                class={checaStatus(n)} 
+                value={i} 
+                disabled={obj.statusBotoesLetras[n]}
+                onClick={() => clicouNaLetra(i)}
+                >    
+                </input>
                 ))
-            };
+            }
         </div>
     );
 }
+
+
+function clicouNaLetra(letra) {
+ console.log("Clicou na letra: "+letra);
+}
+
+
+
+
+
